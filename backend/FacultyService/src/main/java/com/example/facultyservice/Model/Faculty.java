7b5d7851f3680b2959b5aa131e53f69f40614e4c
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-03 00:27:24 +0530

    created Faculty Entity

---
+++ backend/FacultyService/src/main/java/com/example/facultyservice/Model/Faculty.java
@@ -0,0 +1,55 @@
+package com.example.facultyservice.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Faculty {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int f_id;
+    private String name;
+    private String email;
+    private String department;
+
+    public int getF_id() {
+        return f_id;
+    }
+
+    public void setF_id(int f_id) {
+        this.f_id = f_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+}
