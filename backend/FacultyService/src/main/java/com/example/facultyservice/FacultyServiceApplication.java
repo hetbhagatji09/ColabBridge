dff493001bdb1f8d217c07e15f9e935b42333db1
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-03 00:21:55 +0530

    configure the project

--- backend/FacultyService/src/main/java/com/example/facultyservice/FacultyServiceApplication.java
+++ backend/FacultyService/src/main/java/com/example/facultyservice/FacultyServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.facultyservice;
 
+import com.netflix.discovery.EurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FacultyServiceApplication {
 
