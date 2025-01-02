956be8aa5be8290549d10a9289f8d02a5a6176b7
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-03 00:09:30 +0530

    start the admin service

--- backend/AdminService/src/main/java/com/example/adminservice/AdminServiceApplication.java
+++ backend/AdminService/src/main/java/com/example/adminservice/AdminServiceApplication.java
@@ -2,13 +2,17 @@ package com.example.adminservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServiceApplication.class, args);
-		System.out.println("Hello World");
 	}
 
 }
