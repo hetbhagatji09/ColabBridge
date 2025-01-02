b4f069e1260b901281f34a18700e76c805d0cf40
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-03 00:26:19 +0530

    created dao interface for faculty register

---
+++ backend/FacultyService/src/main/java/com/example/facultyservice/Dao/FacultyDao.java
@@ -0,0 +1,10 @@
+package com.example.facultyservice.Dao;
+
+import com.example.facultyservice.Model.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FacultyDao extends JpaRepository<Faculty, Integer> {
+
+}
