39998c9a4e37adc6f0737b7f354118c72affb010
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-03 00:35:31 +0530

    feat: Add FacultyClient interface for communication with FacultyService via Feign

---
+++ backend/AdminService/src/main/java/com/example/adminservice/Feign/FacultyClient.java
@@ -0,0 +1,14 @@
+package com.example.adminservice.Feign;
+
+import com.example.adminservice.Model.Faculty;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "FACULTYSERVICE")
+public interface FacultyClient {
+    @PostMapping("faculty/register")
+    public ResponseEntity<Faculty> registerFaculty(@RequestBody Faculty faculty);
+
+}
