faeb882978587f8ec6d33b9cee3c48b1a6bba0ef
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-03 13:58:35 +0530

    made  project entity

---
+++ backend/Project-Service/src/main/java/com/example/projectservice/Model/Project.java
@@ -0,0 +1,68 @@
+package com.example.projectservice.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int p_id;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDate date;
+
+
+    public int getP_id() {
+        return p_id;
+    }
+
+    public void setP_id(int p_id) {
+        this.p_id = p_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Project(int p_id, String title, String description, String status, LocalDate date) {
+        this.p_id = p_id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.date = LocalDate.now();
+    }
+}
