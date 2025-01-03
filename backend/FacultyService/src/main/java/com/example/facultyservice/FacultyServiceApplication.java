153c2064734a4bb0b3b3dcaca54645d9168fefdb
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-03 16:38:24 +0530

    give annotation to feign client

--- backend/FacultyService/src/main/java/com/example/facultyservice/FacultyServiceApplication.java
+++ backend/FacultyService/src/main/java/com/example/facultyservice/FacultyServiceApplication.java
@@ -4,8 +4,9 @@ import com.netflix.discovery.EurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class FacultyServiceApplication {
 
