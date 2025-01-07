39b29f18bc2afe56827b77e2fc72e3a87d5dbf1f
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-07 19:35:48 +0530

    removed unneccesary imports

--- backend/Project-Service/src/main/java/com/example/projectservice/Service/ProjectService.java
+++ backend/Project-Service/src/main/java/com/example/projectservice/Service/ProjectService.java
@@ -5,15 +5,25 @@ import com.example.projectservice.Model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectService {
     @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+    @Autowired
     private ProjectDao projectDao;
     public ResponseEntity<Project> createProject(Project project) {
         try {
-            return new ResponseEntity<>(projectDao.save(project), HttpStatus.OK);
+//            return new ResponseEntity<>(projectDao.save(project), HttpStatus.OK);
+            ResponseEntity<Project> response = new ResponseEntity<>(projectDao.save(project), HttpStatus.OK);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                messagingTemplate.convertAndSend("/topic/notify-students",
+                        "New project created: " + project.getTitle());
+                System.out.println("Project created: " + project.getTitle());
+            }
+            return response;
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- backend/StudentService/src/main/java/com/example/studentservice/Controller/StudentController.java
+++ backend/StudentService/src/main/java/com/example/studentservice/Controller/StudentController.java
@@ -2,18 +2,13 @@ package com.example.studentservice.Controller;
 
 import com.example.studentservice.Model.Student;
 import com.example.studentservice.Service.StudentService;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("student")
