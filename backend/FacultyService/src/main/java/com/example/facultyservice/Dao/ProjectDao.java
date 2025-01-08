ba1d667993795da1b150137b7efccf42a78aaa87
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-08 09:44:41 +0530

    Defines JPQL for fetching projects

--- backend/FacultyService/src/main/java/com/example/facultyservice/Dao/ProjectDao.java
+++ backend/FacultyService/src/main/java/com/example/facultyservice/Dao/ProjectDao.java
@@ -2,6 +2,14 @@ package com.example.facultyservice.Dao;
 
 import com.example.facultyservice.Model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProjectDao extends JpaRepository<Project,Integer> {
+
+    @Query("SELECT p FROM Project p WHERE p.faculty.f_id = :facultyId")
+    List<Project> findByFacultyId(@Param("facultyId") int facultyId);
+
 }
