7a560826efa3f15ddcd1dc2399d62f2051752164
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-08 09:52:57 +0530

    Created api for create project by faculty

--- backend/FacultyService/src/main/java/com/example/facultyservice/Controller/ProjectController.java
+++ backend/FacultyService/src/main/java/com/example/facultyservice/Controller/ProjectController.java
@@ -4,25 +4,19 @@ import com.example.facultyservice.Model.Project;
 import com.example.facultyservice.Service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("project")
+@RequestMapping("faculty")
 public class ProjectController {
     @Autowired
     private ProjectService projectService;
-    @PostMapping("project")
-    public ResponseEntity<Project> createProject(@RequestBody Project project) {
-        System.out.println("Hello ProjectController");
-        return projectService.createProject(project);
+    @PostMapping("{facultyId}")
+    public ResponseEntity<Project> createProject(@PathVariable int facultyId, @RequestBody Project project) {
+//        System.out.println("value is "+facultyId); // this is just for debugging
+        return projectService.createProject(project, facultyId);
     }
-//    @GetMapping("projects/{f_id}")
-//    public ResponseEntity<List<Project>> getAllProjectsByFacultyId(@PathVariable int f_id){
-//        return projectService.getAllProjectsByFacultyId(f_id);
-//    }
+
 
 
 }
--- backend/FacultyService/src/main/java/com/example/facultyservice/Service/ProjectService.java
+++ backend/FacultyService/src/main/java/com/example/facultyservice/Service/ProjectService.java
@@ -1,6 +1,8 @@
 package com.example.facultyservice.Service;
 
+import com.example.facultyservice.Dao.FacultyDao;
 import com.example.facultyservice.Dao.ProjectDao;
+import com.example.facultyservice.Model.Faculty;
 import com.example.facultyservice.Model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,10 +16,22 @@ public class ProjectService {
     private SimpMessagingTemplate messagingTemplate;
     @Autowired
     private ProjectDao projectDao;
-    public ResponseEntity<Project> createProject(Project project) {
+    @Autowired
+    private FacultyDao facultyDao;
+
+    public ResponseEntity<Project> createProject(Project project, int facultyId) {
         try {
-//            return new ResponseEntity<>(projectDao.save(project), HttpStatus.OK);
+            Faculty faculty = facultyDao.findById(facultyId).get();
+            System.out.println("Faculty: " + faculty);
+            if(faculty == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            project.setFaculty(faculty);
+
             ResponseEntity<Project> response = new ResponseEntity<>(projectDao.save(project), HttpStatus.OK);
+
+            System.out.println("Hii Bro");
             if (response.getStatusCode().is2xxSuccessful()) {
                 messagingTemplate.convertAndSend("/topic/notify-students",
                         "New project created: " + project.getTitle());
@@ -25,6 +39,7 @@ public class ProjectService {
             }
             return response;
         } catch (Exception e) {
+            System.out.println("Error in ProjectService: " + e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
