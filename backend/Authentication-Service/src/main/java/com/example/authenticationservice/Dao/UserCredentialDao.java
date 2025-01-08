0a7bb957adab819155b162a55cc9dfba6a1bffe3
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-08 23:29:19 +0530

    Define method for fetch user by username

--- backend/Authentication-Service/src/main/java/com/example/authenticationservice/Dao/UserCredentialDao.java
+++ backend/Authentication-Service/src/main/java/com/example/authenticationservice/Dao/UserCredentialDao.java
@@ -3,5 +3,8 @@ package com.example.authenticationservice.Dao;
 import com.example.authenticationservice.Model.UserCredential;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserCredentialDao extends JpaRepository<UserCredential,Integer> {
+    Optional<UserCredential> findByUsername(String username);
 }
