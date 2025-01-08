a60cfcdba7c6d79916e6724d4033bb84d653c6d6
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-08 23:34:36 +0530

    Created api for delete project by faculty

--- backend/FacultyService/src/main/java/com/example/facultyservice/Controller/FacultyController.java
+++ backend/FacultyService/src/main/java/com/example/facultyservice/Controller/FacultyController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("faculty")
 public class FacultyController {
@@ -18,8 +20,18 @@ public class FacultyController {
     public ResponseEntity<Faculty> registerFaculty(@RequestBody Faculty faculty) {
         return facultyService.registerFaculty(faculty);
     }
-    @PostMapping("/project")
-    public ResponseEntity<Project>createProject(@RequestBody Project project){
-         return facultyService.createProject(project);
+    @GetMapping("{f_id}")
+    ResponseEntity<List<Project>>getProjectsById(@PathVariable int f_id){
+        return facultyService.getProjectsById(f_id);
     }
+    @GetMapping("projectsName/{f_id}")
+    ResponseEntity<List<String>>getProjectsByName(@PathVariable int f_id){
+        return facultyService.getProjectsByName(f_id);
+    }
+    @DeleteMapping("/project/{p_id}")
+    ResponseEntity<String>deleteProject(@PathVariable int p_id){
+        return facultyService.deleteProject(p_id);
+    }
+
+
 }
--- backend/FacultyService/src/main/java/com/example/facultyservice/Service/FacultyService.java
+++ backend/FacultyService/src/main/java/com/example/facultyservice/Service/FacultyService.java
@@ -1,6 +1,7 @@
 package com.example.facultyservice.Service;
 
 import com.example.facultyservice.Dao.FacultyDao;
+import com.example.facultyservice.Dao.ProjectDao;
 import com.example.facultyservice.Model.Faculty;
 import com.example.facultyservice.Model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +9,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class FacultyService {
     @Autowired
     private FacultyDao facultyDao;
+    @Autowired
+    private ProjectDao projectDao;
     @Autowired
     private ProjectService projectService;
 
@@ -25,13 +31,50 @@ public class FacultyService {
 
     }
 
+    public ResponseEntity<List<Project>> getProjectsById(int facultyId) {
+        try{
+            Faculty faculty=facultyDao.findById(facultyId).get();
+            List<Project>projects=projectDao.findByFacultyId(faculty.getF_id());
+            return new ResponseEntity<>(projects,HttpStatus.OK);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-    public ResponseEntity<Project> createProject(Project project) {
+    }
+
+    public ResponseEntity<List<String>> getProjectsByName(int fId) {
         try {
-            return projectService.createProject(project);
-        } catch (Exception e) {
-            System.err.println("Error in FacultyService: " + e.getMessage());
+            Faculty faculty = facultyDao.findById(fId).get();
+            List<Project> projects = projectDao.findByFacultyId(faculty.getF_id());
+            List<String> projectNames = new ArrayList<>();
+            for (Project project : projects) {
+                projectNames.add(project.getTitle());
+            }
+            return new ResponseEntity<>(projectNames, HttpStatus.OK);
+        }
+        catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    public ResponseEntity<String> deleteProject(int p_id) {
+        try {
+            projectDao.deleteById(p_id);
+            return new ResponseEntity<>("Project deleted successfully", HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+//    public ResponseEntity<Project> createProject(Project project,int f_id) {
+//        try {
+//            return projectService.createProject(project, f_id);
+//        } catch (Exception e) {
+//            System.err.println("Error in FacultyService: " + e.getMessage());
+//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 }
