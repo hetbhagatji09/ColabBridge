6cab2481d5d7a059e29d7d3669b59ccddaccc1f5
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-13 13:57:57 +0530

    add lombok dependency and give annotations to model file

--- backend/StudentService/src/main/java/com/example/studentservice/Model/Project.java
+++ backend/StudentService/src/main/java/com/example/studentservice/Model/Project.java
@@ -2,10 +2,17 @@ package com.example.studentservice.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDate;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@ToString
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Project {
     private int p_id;
     private String title;
@@ -18,76 +25,5 @@ public class Project {
     @JoinColumn(name = "facultyId")
     private Faculty faculty;
 
-    public Faculty getFaculty() {
-        return faculty;
-    }
 
-    public void setFaculty(Faculty faculty) {
-        this.faculty = faculty;
-    }
-
-
-
-    public int getP_id() {
-        return p_id;
-    }
-
-    public void setP_id(int p_id) {
-        this.p_id = p_id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public Project(int p_id, String title, String description, Status status, LocalDate date, Faculty faculty) {
-        this.p_id = p_id;
-        this.title = title;
-        this.description = description;
-        this.status = status;
-        this.date = date;
-        this.faculty = faculty;
-    }
-
-    @Override
-    public String toString() {
-        return "Project{" +
-                "p_id=" + p_id +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", status=" + status +
-                ", date=" + date +
-                ", faculty=" + faculty +
-                '}';
-    }
-
-    public Project() {
-        this.date=LocalDate.now();
-    }
 }
