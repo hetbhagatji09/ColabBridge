c323763ba728199ee34a14337cbb1d1866b8083e
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-01-13 14:20:06 +0530

    Modified project files

--- backend/FacultyService/src/main/java/com/example/facultyservice/Model/Project.java
+++ backend/FacultyService/src/main/java/com/example/facultyservice/Model/Project.java
@@ -2,11 +2,8 @@ package com.example.facultyservice.Model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Getter
