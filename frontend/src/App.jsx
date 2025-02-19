f2a313cfd721038b9fa66fdac5ac594d6c24736a
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-02-19 09:16:56 +0530

    add routes for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,14 @@ import { AuthProvider, useAuth } from './contexts/AuthContext';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ProjectDetails from './pages/ProjectDetails';
-import ConfirmedProjects from './pages/ConfirmedProjects';
+import ConfirmedProjects from './components/Faculty/ConfirmedProjects';
 import Feedback from './pages/Feedback';
 import Layout from './components/Layout';
-
+import FacultyDashboard from './components/Faculty/FacultyDashboard';
+import ProjectDetailsPage from './components/Faculty/ProjectDetailsPage';
+import Home from './pages/Home';
+import StudentProjectCard from './components/StudentProjectCard';
+import StudentDetail from './components/Faculty/StudentDetail';
 // Protected Route wrapper component
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
@@ -25,6 +29,7 @@ function App() {
           <AnimatePresence mode="wait">
             <Routes>
               <Route path="/login" element={<Login />} />
+              <Route path="/" element={<Home />} />
               <Route
                 path="/"
                 element={
@@ -33,7 +38,11 @@ function App() {
                   </ProtectedRoute>
                 }
               >
-                <Route path='dashboard' element={<Dashboard />} />
+                <Route path='student/dashboard' element={<Dashboard />} />
+                <Route path="/project/:projectId" element={<ProjectDetailsPage/>}/>
+                <Route path="/application/project/:projectId" element={<StudentDetail/>}/>
+                <Route path="/studentproject/:projectId" element={<ProjectDetails/>}/>
+                <Route path="faculty/dashboard" element={<FacultyDashboard />} />
                 <Route path="project/:id" element={<ProjectDetails />} />
                 <Route path="confirmed-projects" element={<ConfirmedProjects />} />
                 <Route path="feedback" element={<Feedback />} />
