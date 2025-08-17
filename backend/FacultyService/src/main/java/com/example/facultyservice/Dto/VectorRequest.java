011af6aec015de1a3a78f5035a6e59ac8dd5d440
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-08-17 20:49:13 +0530

    Revert "Added google api key"
    This reverts commit fbdb4fff2ba1e2979bf53822158ac7a0317bee97.

--- backend/FacultyService/src/main/java/com/example/facultyservice/Dto/VectorRequest.java
+++ backend/FacultyService/src/main/java/com/example/facultyservice/Dto/VectorRequest.java
@@ -1,14 +1,4 @@
 package com.example.facultyservice.Dto;
 
-import lombok.*;
-
-@Data
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class VectorRequest {
-    private int projectId;
-    private String content;
-
 }
